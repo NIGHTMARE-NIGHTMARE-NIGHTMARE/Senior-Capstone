257a1426a45f3c337dee273cdad8518c090fd6f6
Carson-Spaniel <carsonspaniel@gmail.com>
2024-04-06 17:35:04 -0500

    styling

--- django/iBIS_App/static/js/script.js
+++ django/iBIS_App/static/js/script.js
@@ -3,7 +3,7 @@ function sendMessage() {
     const area = document.querySelector(".message-area");
     
 
-    fetch(`http://100.70.102.193:5005/webhooks/rest/webhook`, {
+    fetch(`http://10.10.19.11:5005/webhooks/rest/webhook`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -54,6 +54,45 @@ function sendMessage() {
         message.value = "";
     })
     .catch(error => {
-        console.error("Error:", error);
+        const main = document.querySelector("main");
+
+    console.error("Error:", error);
+        const errorMessage = document.createElement('div');
+        const header = document.createElement('h2');
+        const errorText = document.createElement('p');
+        header.textContent = "An Error Occurred";
+        errorText.textContent = "Try again in 5 seconds";
+
+        errorMessage.appendChild(header);
+        errorMessage.appendChild(errorText);
+
+        errorMessage.className = `error-message`;
+        errorMessage.style.opacity = "0"; // Start with opacity 0
+        main.appendChild(errorMessage);
+
+        let i = 4;
+
+        const fadeIn = setInterval(() => {
+            let opacity = parseFloat(errorMessage.style.opacity);
+            errorMessage.style.opacity = (opacity + 0.1).toString();
+
+            if (opacity >= 1) {
+                clearInterval(fadeIn);
+
+                const countdown = setInterval(() => {
+                    errorText.textContent = `Try again in ${i} seconds`;
+                    i--;
+
+                    if (i < 1) {
+                        clearInterval(countdown);
+                        errorMessage.style.transition = "opacity 1s";
+                        errorMessage.style.opacity = "0";
+                        setTimeout(() => {
+                            main.removeChild(errorMessage);
+                        }, 1000); // Wait for the transition to complete before removing
+                    }
+                }, 1000);
+            }
+        }, 0);
     });
 }
