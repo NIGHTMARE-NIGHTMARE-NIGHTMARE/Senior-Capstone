ae2be3cea9a1d8c6affa99614fbef33241dcc812
Asgideon <79775633+Asgideon@users.noreply.github.com>
2024-04-09 23:55:56 -0500

    Changes for about.html

--- django/iBIS_App/urls.py
+++ django/iBIS_App/urls.py
@@ -3,6 +3,9 @@ from iBIS_App import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("api/", views.api_call, name='api')
+    path("api/", views.api_call, name='api'),
+    path('about/',views.about, name='about'),
+    path('home/',views.home, name='home'),
+
 ]
 
--- django/iBIS_App/views.py
+++ django/iBIS_App/views.py
@@ -16,6 +16,16 @@ def home(request):
     }
     return render(request, "home.html", context)
 
+def about(request):
+    host_url = f'http://{request.get_host()}'
+    csrf_token = get_csrf_token(request)
+
+    context = {
+    'csrf_token': csrf_token,
+    'host': host_url,
+    }
+    return render(request, "about.html", context)
+
 # Function to get CSRF token from the request
 def get_csrf_token(request):
     return request.COOKIES.get('csrftoken', '')
